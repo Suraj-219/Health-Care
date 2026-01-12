ba8561666468fb2b8084538dc21312dc333169d8
Suraj Pal <surajpal7442@gmail.com>
2026-01-12 10:14:40 +0530

    Add routing and create initial page components for the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctor" element={<Doctor />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/myappointment" element={<MyAppointment />} />
+        <Route path="/appointment/:doctorId" element={<Appointment />} />
+      </Routes>
     </div>
   )
 }
