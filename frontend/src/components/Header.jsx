402819f8b98f785bd51701181518e72524efb3e0
Suraj Pal <surajpal7442@gmail.com>
2026-01-16 19:55:33 +0530

    update header components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
-        
-        {/* ---left side--- */}
-        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-20 md:mb-[-30px]'>
-            <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
-                Book Appointment <br/> With Trusted Doctors
-            </p>
-            <div>
-                <img src={assets.group_profiles} alt="" />
-                <p>Simply browse through our extensive list of trusted doctors, <br/> schedule your appointment hassle-free.</p>
-            </div>
-            <a href="">
-                Book appointment <img src={assets.arrow_icon} alt="" />
-            </a>
+    <div className="flex flex-col md:flex-row bg-primary rounded-lg px-6 md:px-12 lg:px-20 py-10 md:py-0 overflow-hidden">
+
+      {/* -------- Left Section -------- */}
+      <div className="md:w-1/2 flex flex-col justify-center gap-6 md:py-20">
+
+        <p className="text-3xl md:text-4xl lg:text-5xl font-semibold text-white leading-snug">
+          Book Appointment <br /> With Trusted Doctors
+        </p>
+
+        <div className="flex items-center gap-3">
+          <img
+            src={assets.group_profiles}
+            alt=""
+            className="w-24"
+          />
+          <p className="text-white text-sm md:text-base">
+            Simply browse through our extensive list of trusted doctors,
+            schedule your appointment hassle-free.
+          </p>
         </div>
 
-        {/* ---right side--- */}
-        <div className='md:w-1/2 relative flex justify-end'>
-            <img className='w-full md:w-[90%] md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
-        </div>
+        <a
+          href="#speciality"
+          className="flex items-center gap-2 bg-white text-primary px-6 py-3 rounded-full w-fit font-medium hover:scale-105 transition"
+        >
+          Book appointment
+          <img src={assets.arrow_icon} alt="" className="w-4" />
+        </a>
+      </div>
+
+      {/* -------- Right Section -------- */}
+      <div className="md:w-1/2 relative flex justify-end">
+        <img
+          src={assets.header_img}
+          alt=""
+          className="w-full md:w-[90%] md:absolute bottom-0"
+        />
+      </div>
+
     </div>
   )
 }
