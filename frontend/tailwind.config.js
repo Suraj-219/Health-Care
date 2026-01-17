51ceaa6d3a7a96aad33c62f8d204c948a170ea39
Suraj Pal <surajpal7442@gmail.com>
2026-01-17 15:29:58 +0530

    Add TopDoctors components

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors: {
         'primary': "#3bae79"
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))',
       }
     },
   },
