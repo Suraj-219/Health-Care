04d8194f33434fabb72346ce67b85945116696df
Suraj Pal <surajpal7442@gmail.com>
2026-01-19 10:10:22 +0530

    write logic for date and time

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -9,15 +9,69 @@ const Appointment = () => {
   const {doctors, currencySymbol} = useContext(AppContext)
 
   const [docInfo, setDocInfo] = useState(null)
+  const [docSlots, setDocSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSloteTime] = useState('')
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc.id === docId)
     setDocInfo(docInfo)
   }
 
+  const getAvailableSlots = async ()=> {
+    setDocSlots([])
+
+    // getting current data
+    let today = new Date()
+
+    for(let i=0; i<7; i++){
+      // getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i)
+
+      // setting end time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+i)
+      endTime.setHours(21,0,0,0)
+
+      // setting hours
+      if(today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while(currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+
+        // add slot to array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime
+        })
+
+        // Increment current time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
+      }
+      setDocSlots(prev=> ([...prev, timeSlots]))
+    }
+  }
+
   useEffect(()=>{
     fetchDocInfo()
   }, [doctors, docId])
+
+  useEffect(()=>{
+    getAvailableSlots()
+  }, [docInfo])
+
+  useEffect(()=>{
+    console.log(docSlots)
+  }, [docSlots])
 
   return docInfo && (
     <div>
