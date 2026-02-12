f4d44045b0bb7e77a77e7383fa8e9cb4c87f4778
Suraj Pal <surajpal7442@gmail.com>
2026-02-12 20:49:27 +0530

    Enhanced Sidebar components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
-          <Route path='all-appointements' element={<AllApointments />} />
+          <Route path='/all-appointements' element={<AllApointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctor-list' element={<DoctorList />} />
         </Routes>
