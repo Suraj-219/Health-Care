7fc577e5edf099aa48e3226c66bcd07bc42742a7
Suraj Pal <surajpal7442@gmail.com>
2026-03-18 18:52:32 +0530

    Add DoctorContext integration for token management in Login and App components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,12 +10,14 @@ import Dashboard from './pages/Admin/Dashboard';
 import AllApointments from './pages/Admin/AllApointments';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorList from './pages/Admin/DoctorList';
+import { DoctorContext } from './context/DoctorContext';
 
 const App = () => {
 
   const {aToken} = useContext(AdminContext);
+  const {dToken} = useContext(DoctorContext)
 
-  return aToken ? (
+  return aToken || dToken ? (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar />
